2ce3cb8a68940d6359d125c60135c018e55b5ca0
margarita <107617699+margarita2104@users.noreply.github.com>
2024-08-20 15:39:37 +0200

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="relative min-h-screen flex">
       <h1 className="text-6xl font-bold font-montserrat mt-28 w-2/3">
-        HI, I&rsquo;M&nbsp;MARGARITA, FRONTEND DEVELOPER
+        HI, I&rsquo;M&nbsp;MARGARITA, SOFTWARE DEVELOPER
       </h1>
       <img
         className="absolute -top-24 -right-20 -z-10 max-w-4xl"
