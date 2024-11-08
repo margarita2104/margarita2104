530f26a57b59d69329da86285c4132509d8ae4a7
margarita2104 <margarita.p.ferreira@gmail.com>
2024-11-08 18:58:39 +0100

    created a navlink component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,60 +1,36 @@
 import { Link, animateScroll as scroll } from "react-scroll";
 
+const OFFSET_VALUES = {
+  home: -70,
+  about: -200,
+  projects: -100,
+  education: -100,
+  contact: -250
+};
+
+const NavLink = ({ to, offset, label }) => (
+  <Link
+    className="hover:text-bright-sun"
+    to={to}
+    spy={true}
+    smooth={true}
+    offset={offset}
+    duration={500}
+  >
+    {label}
+  </Link>
+);
+
 const Header = () => {
   return (
     <header className="my-4">
       <nav className="py-4 px-6 border-2 rounded-full">
         <ul className="flex md:gap-10 gap-4 md:justify-start justify-between">
-          <Link
-            className="hover:text-bright-sun"
-            to="#"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-          >
-            Home
-          </Link>
-          <Link
-            className="hover:text-bright-sun"
-            to="about"
-            spy={true}
-            smooth={true}
-            offset={-200}
-            duration={500}
-          >
-            About
-          </Link>
-          <Link
-            className="hover:text-bright-sun"
-            to="projects"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-          >
-            Projects
-          </Link>
-          <Link
-            className="hover:text-bright-sun"
-            to="education"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-          >
-            Education
-          </Link>
-          <Link
-            className="hover:text-bright-sun"
-            to="contact"
-            spy={true}
-            smooth={true}
-            offset={-250}
-            duration={500}
-          >
-            Contact
-          </Link>
+          <NavLink to="#" offset={OFFSET_VALUES.home} label="Home" />
+          <NavLink to="about" offset={OFFSET_VALUES.about} label="About" />
+          <NavLink to="projects" offset={OFFSET_VALUES.projects} label="Projects" />
+          <NavLink to="education" offset={OFFSET_VALUES.education} label="Education" />
+          <NavLink to="contact" offset={OFFSET_VALUES.contact} label="Contact" />
         </ul>
       </nav>
     </header>
